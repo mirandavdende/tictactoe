9ae5a088aba35240ec919076ee92ff57e7e8ff75
Timendus <mail@timendus.com>
2020-04-24 17:28:31 +0200

    Add user input and putting down pieces

--- classes/index.js
+++ classes/index.js
@@ -1,10 +1,12 @@
 const { Pieces, Board } = require('./board');
 const { Cursor } = require('./cursor');
 const { View } = require('./view');
+// ...
 
 module.exports = {
   Pieces,
   Board,
   Cursor,
-  View
+  View,
+  Input
 };
--- local.js
+++ local.js
@@ -1,13 +1,36 @@
 #!/usr/bin/env node
 
-const { Pieces, Board, Cursor, View } = require('./classes');
+const { Pieces, Board, Cursor, View, Input } = require('./classes');
 
 const board  = new Board();
 const cursor = new Cursor(board);
 const view   = new View();
+const input  = new Input();
 
-// Set the piece at coordinates (0,1) to be a cross
-board.set(0, 1, Pieces.Cross);
+let turn = Pieces.Cross;
 
-// Show what's in memory
-view.render(board, cursor);
+input.addKeyListener(key => {
+  switch(key) {
+    case 'up':
+      return cursor.up();
+    case 'down':
+      return cursor.down();
+    case 'left':
+      // ..
+    case 'right':
+      // ..
+
+    case 'enter':
+      const pieceUnderCursor = board.get(cursor.x(), cursor.y());
+      if ( pieceUnderCursor == Pieces.Clear ) {
+        board.set(cursor.x(), cursor.y(), turn);
+
+        // ... switch turn..?
+      }
+      return;
+  }
+});
+
+setInterval(() => {
+  view.render(board, cursor);
+}, 33);
